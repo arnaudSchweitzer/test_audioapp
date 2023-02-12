38b7597189f176d49b68673b50d7cca22708c303
Your Name <you@example.com>
2023-02-12 17:06:41 +0100

    Main

---
+++ lib/Views/playlists_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class _PlaylistShow extends StatelessWidget {
+  const _PlaylistShow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
